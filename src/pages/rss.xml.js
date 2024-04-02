07caca8a2589d833f53060d5d1d4944f02c0fd1e
Zaclend <spaisenok@gmail.com>
2024-04-02 11:30:29 +0300

    RssNew

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function get() {
   return rss({
     title: 'Astro Learner | Blog',
-    description: 'Моё познание Астро',
+    description: 'Моё познание ',
     site: 'https://pizdecdog.netlify.app/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
