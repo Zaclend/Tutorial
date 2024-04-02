9136be89fcc6d4341e6d218e162931beb3a83d74
Zaclend <spaisenok@gmail.com>
2024-04-02 15:52:01 +0300

    ClickButton

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function get() {
   return rss({
     title: 'Astro Learner | Blog',
-    description: 'Моё познание ',
+    description: 'My journey learning Astro',
     site: 'https://pizdecdog.netlify.app/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
